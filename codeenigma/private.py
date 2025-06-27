ceb489fd9b5141d836bd03937fae04572204694c
Krishnan S G <krishsg525@gmail.com>
2025-06-27 12:17:45 +0530

    enh: Use unique key for every run

---
+++ codeenigma/private.py
@@ -0,0 +1,7 @@
+
+import os
+
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+NONCE = os.urandom(12)  # randomly selected 96-bit nonce
+SECRET_KEY = key = AESGCM.generate_key(bit_length=256)
