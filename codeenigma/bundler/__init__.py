25e99d9e998db96c6323348515c9d72b695f36f1
Krishnan S G <krishsg525@gmail.com>
2025-07-04 14:21:48 +0530

    refactor: Create new interface for bundling wheels and extensions for poetry and setuptools

---
+++ codeenigma/bundler/__init__.py
@@ -0,0 +1,5 @@
+from codeenigma.bundler.poetry import PoetryBundler
+from codeenigma.bundler.standard import StandardBundler
+from codeenigma.bundler.base import IBundler
+
+__all__ = ["PoetryBundler", "StandardBundler", "IBundler"]
